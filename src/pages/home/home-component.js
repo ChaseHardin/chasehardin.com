3f3bb54cae383e41140aea622fbdbcfb3258a428
Chase Hardin <chase.hardin@scribbletechnology.com>
2019-02-10 13:35:21 -0600

    Make image responsive for different devices.

--- src/pages/home/home-component.js
+++ src/pages/home/home-component.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import './home-component.css'
-import WorkingOnComputer from '../../resources/images/working_on_computer.jpg';
-
-const image = {
-    width: "100%",
-    height: "800px",
-    backgroundSize: "cover",
-    backgroundImage: "url(" + WorkingOnComputer + ")",
-    backgroundRepeat: "no-repeat"
-}
 
 export default function Home() {
     return (
         <div className='home'>
-            {/* <h1>Home</h1> */}
-            <section style={image}></section>
+            <section className='working-on-computer'>
+            </section>
         </div>
     );
 }
