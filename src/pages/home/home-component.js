157ac035381b9d9024e1bcb2effc9341ddd9a0f3
Chase Hardin <chase.hardin@scribbletechnology.com>
2019-02-10 13:14:48 -0600

    Add image to home page.

--- src/pages/home/home-component.js
+++ src/pages/home/home-component.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './home-component.css'
+import WorkingOnComputer from '../../resources/images/working_on_computer.jpg';
+
+const image = {
+    width: "100%",
+    height: "800px",
+    backgroundSize: "cover",
+    backgroundImage: "url(" + WorkingOnComputer + ")",
+    backgroundRepeat: "no-repeat"
+}
 
 export default function Home() {
     return (
-        <div className='home container-fluid'>
-            <h1>Home</h1>
+        <div className='home'>
+            {/* <h1>Home</h1> */}
+            <section style={image}></section>
         </div>
     );
 }
